f1194b84a132e91c89cb20fff9dba58dc4307df7
Ricky Ayoub <vgmoose@gmail.com>
2016-02-01 00:51:38 -0500

    Bitmap experiments + pong example

---
+++ src/program.h
@@ -0,0 +1,14 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+#include "../../../libwiiu/src/coreinit.h"
+#include "../../../libwiiu/src/vpad.h"
+#include "../../../libwiiu/src/types.h"
+//Using modified version of draw to render at twice the scale to improve framerate
+#include "draw.h"
+
+#include "pong.h"
+
+void _entryPoint();
+
+#endif /* PROGRAM_H */
