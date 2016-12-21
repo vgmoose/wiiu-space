29014bf5fd28ce2c0797a17af43b69b554c4690e
CreeperMario Games <creepermario2@gmail.com>
2016-12-22 09:21:18 +1030

    Updated scripts for macOS and removed directory data
    * I modified dimok's reosurce generator script (filelist.sh) so that it works when running under Darwin platforms (such as macOS). It should still work with Windows and Linux platforms, as I haven't changed any of the Windows/Linux code and all my new code is surrounded by "if $OSTYPE == darwin".
    * I removed the .DS_Store files from my previous commit (was a pretty stupid move of me.)

--- filelist.sh
+++ filelist.sh
@@ -7,24 +7,14 @@ outFile="./src/resources/filelist.h"
 count_old=$(cat $outFile 2>/dev/null | tr -d '\n\n' | sed 's/[^0-9]*\([0-9]*\).*/\1/')
 
 count=0
-if [[ $OSTYPE == darwin* ]];
-then
 
-for i in $(gfind ./data/images/ ./data/sounds/ ./data/fonts/ -maxdepth 1 -type f  \( ! -printf "%f\n" \) | sort -f)
+for i in $(find ./data/images/ ./data/sounds/ ./data/fonts/ -maxdepth 1 -type f)  #\( ! -printf "%f\n" \) | sort -f)
 do
 	files[count]=$i
 	count=$((count+1))
 done
 
-else
-
-for i in $(find ./data/images/ ./data/sounds/ ./data/fonts/ -maxdepth 1 -type f  \( ! -printf "%f\n" \) | sort -f)
-do
-	files[count]=$i
-	count=$((count+1))
-done
-
-fi
+#fi
 
 if [ "$count_old" != "$count" ] || [ ! -f $outFile ]
 then
@@ -57,6 +47,21 @@ for i in ${files[@]}
 do
 	filename=${i%.*}
 	extension=${i##*.}
+
+  # For some reason, macOS and other Darwin-based operating systems contain a
+  # find untility that outputs full file paths instead of singular file names.
+  # To prevent this from screwing our program, we will remove the paths from the
+  # entries returned by the above loop if we are running on a Darwin platform.
+  if [[ $OSTYPE == darwin* ]]; then
+    if [[ ${filename:0:15} == ./data/sounds// ]]; then
+      filename=${filename:15}
+    elif [[ ${filename:0:15} == ./data/images// ]]; then
+      filename=${filename:15}
+    elif [[ ${filename:0:14} == ./data/fonts// ]]; then
+      filename=${filename:14}
+    fi
+  fi
+
 	echo 'extern const unsigned char '$filename'_'$extension'[];' >> $outFile
 	echo 'extern const unsigned int '$filename'_'$extension'_size;' >> $outFile
 	echo '' >> $outFile
@@ -69,7 +74,19 @@ for i in ${files[@]}
 do
 	filename=${i%.*}
 	extension=${i##*.}
-	echo -e '\t{"'$i'", '$filename'_'$extension', '$filename'_'$extension'_size, NULL, 0},' >> $outFile
+
+  # See above for documentation on what this does.
+  if [[ $OSTYPE == darwin* ]]; then
+    if [[ ${filename:0:15} == ./data/sounds// ]]; then
+      filename=${filename:15}
+    elif [[ ${filename:0:15} == ./data/images// ]]; then
+      filename=${filename:15}
+    elif [[ ${filename:0:14} == ./data/fonts// ]]; then
+      filename=${filename:14}
+    fi
+  fi
+
+	echo -e '\t{"'$filename'.'$extension'", '$filename'_'$extension', '$filename'_'$extension'_size, NULL, 0},' >> $outFile
 done
 
 echo -e '\t{NULL, NULL, 0, NULL, 0}' >> $outFile
